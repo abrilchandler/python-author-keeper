6362364fd6d876608e6b2f5fdc7873bed8557e7e
Chandler Abril <abrilchandler1@gmail.com>
2024-08-13 12:23:52 -0700

    New print statement with display books for author

--- lib/helpers.py
+++ lib/helpers.py
@@ -9,9 +9,9 @@ def display_books_for_author():
     if choice - 1 < len(authors) and authors[choice - 1]:
         author = authors[choice - 1]
         books = author.books_by_author()
-        print("Books written by: ")
+        print(f"Books written by {author.name}: ")
         for choice, book in enumerate(books, start = 1):
-            print(f"{choice}. Written by {author.name}: {book.name}, {book.genre}")
+            print(f"{book.name}, {book.genre}")
     else:
         print(f"No books found for {choice}")
 
